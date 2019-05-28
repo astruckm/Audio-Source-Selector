426a4cb059e30aa524dc3a8562277d1b51a1d16b
Andrew Struck-Marcell <astruckm@gmail.com>
2019-05-28 09:13:21 -0700

    Updated to Swift 5

---
+++ Audio Source Selector/Defaults.swift
@@ -0,0 +1,17 @@
+//
+//  Defaults.swift
+//  Audio Source Selector
+//
+//  Created by ASM on 5/16/19.
+//  Copyright © 2019 ASM. All rights reserved.
+//
+
+import Foundation
+
+final class Defaults {
+    let standardDefaults = UserDefaults.standard
+    let selectedInputSource = "selectedInputSource"
+    
+    
+}
+
